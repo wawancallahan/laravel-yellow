76db90f2f5923c16c0eef5e3d547445073de63e0
wawancallahan <wawan.callahan@gmail.com>
2023-08-21 20:00:24 +0800

    add 1 api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('cek-status-pinjaman', function (Request $request) {
+    return response()->json([
+        'status' => 'SUCCESS',
+        'message' => 'SUCCESS',
+        'data' => [
+            'loan_number' => $request->loan_number,
+            'loan_id' => random_int(0, 50000),
+            'tenor' => [
+                12,
+                24,
+                36
+            ][random_int(0, 2)],
+            'jatuh_tempo' => '2023-01-01',
+            'total_pembayaran' => 5000000
+        ]
+    ], 200);
+});
